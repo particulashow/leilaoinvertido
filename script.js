cb148924557a1fd45ce6b3c01d7a0208eb14aba1
particulashow <particula.influente@gmail.com>
2026-01-28 16:07:07 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 let totalSeconds = 60 * 60; // 60 minutos
 const timerEl = document.getElementById("auctionTimer");
-const progressFill = document.getElementById("progressFill");
-
-const totalInitial = totalSeconds;
 
 function updateAuctionTimer() {
   const m = Math.floor(totalSeconds / 60);
   const s = totalSeconds % 60;
 
   timerEl.textContent = `${String(m).padStart(2, "0")}:${String(s).padStart(2, "0")}`;
-
-  // PROGRESS BAR
-  const pct = (totalSeconds / totalInitial) * 100;
-  progressFill.style.width = pct + "%";
 
   // CORES PROGRESSIVAS
   let color = "#00ff88"; // verde
@@ -31,12 +24,10 @@ function updateAuctionTimer() {
   }
 
   timerEl.style.color = color;
-  progressFill.style.background = color;
 
   if (totalSeconds <= 0) {
     timerEl.textContent = "TERMINADO";
     timerEl.style.color = "#ff0000";
-    progressFill.style.background = "#ff0000";
     timerEl.classList.add("blink");
     clearInterval(countdownInterval);
   }
